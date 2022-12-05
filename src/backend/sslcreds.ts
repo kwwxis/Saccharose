40f1b7c7e93057d87b5b045a721fb27d73515e1e
kwwxis <kwwxis@users.noreply.github.com>
2022-12-04 21:02:35 -0800

    Update

--- src/backend/sslcreds.ts
+++ src/backend/sslcreds.ts
@@ -1,10 +1,11 @@
 import fs from 'fs';
 import { ServerOptions } from 'spdy';
+import { toBoolean } from '../shared/util/genericUtil';
 
 export default <ServerOptions> {
-  key: process.env.SSL_ENABLED ? fs.readFileSync(process.env.SSL_KEY, 'utf8') : null,
-  cert: process.env.SSL_ENABLED ? fs.readFileSync(process.env.SSL_CERT, 'utf8') : null,
-  ca: process.env.SSL_ENABLED ? fs.readFileSync(process.env.SSL_CA, 'utf8') : null,
+  key: toBoolean(process.env.SSL_ENABLED) ? fs.readFileSync(process.env.SSL_KEY, 'utf8') : null,
+  cert: toBoolean(process.env.SSL_ENABLED) ? fs.readFileSync(process.env.SSL_CERT, 'utf8') : null,
+  ca: toBoolean(process.env.SSL_ENABLED) ? fs.readFileSync(process.env.SSL_CA, 'utf8') : null,
   spdy: {
     plain: false,
     ssl: !!process.env.SSL_ENABLED,
