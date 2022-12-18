9c06be86ed41d25ad89ff0bac05c9c15d22d3136
kwwxis <kwwxis@users.noreply.github.com>
2022-12-18 06:31:03 -0800

    Update

--- src/backend/controllers/app/AppRouter.ts
+++ src/backend/controllers/app/AppRouter.ts
@@ -185,7 +185,7 @@ export default async function(): Promise<Router> {
     }
 
     res.render('pages/character/character-stories', {
-      title: 'Character Stories - ' + story.avatar.NameText,
+      title: 'Character Stories - ' + (story?.avatar?.NameText || 'N/A'),
       avatarId: req.params.avatarId,
       story: story,
       bodyClass: ['page--character-stories'],
