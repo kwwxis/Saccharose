4f11a18afcce258066ebeb2f8aa56646f0174180
kwwxis <kwwxis@users.noreply.github.com>
2023-01-05 02:41:16 -0800

    Fix?

--- src/backend/scripts/OLgen/OLgen.ts
+++ src/backend/scripts/OLgen/OLgen.ts
@@ -190,7 +190,9 @@ export function highlight_ol_differences(olResults: OLResult[]): OLResult[] {
         param.setValue(escapeHtml(param.value));
       }
     }
-    olResult.result = olResult.templateNode.toString();
+    olResult.result = olResult.templateNode.toString().replace(/  +/g, match => {
+      return '&nbsp;'.repeat(match.length);
+    });
       // .split('\n')
       // .map(line => `<div>${line}</div>`)
       // .join('\n');
