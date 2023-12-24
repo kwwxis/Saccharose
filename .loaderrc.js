7b1885cb19f57876d1c76f7e5e7cb065fb4f94da
kwwxis <118708201+kwwxis@users.noreply.github.com>
2023-12-23 22:25:51 -0800

    Update

--- .loaderrc.js
+++ .loaderrc.js
@@ -1,15 +1,44 @@
+import ts from 'typescript';
+import path from 'node:path';
+import process from 'node:process';
+
+let tsConfig;
+const vueTsRegex = /lang=ts/;
+
 export default {
   loaders: [
+    'esm-loader-typescript',
+    'vue-esm-loader',
     {
-      loader: 'esm-loader-typescript',
-      options: {
-        files: [/\.tsx?$/, /\.vue.*lang=ts/]
-      }
-    },
-    {
-      loader: 'vue-esm-loader',
-      options: {
-        files: [/\.vue$/]
+      resolve(specifier, ctx) {
+        if (!vueTsRegex.test(specifier)) {
+          return;
+        }
+        return {
+          format: 'module',
+          url: new URL(specifier, ctx.parentURL).href,
+        };
+      },
+      transform(source, ctx) {
+        if (!vueTsRegex.test(ctx.url)) {
+          return;
+        }
+
+        if (!tsConfig) {
+          const configFileName = ts.findConfigFile(process.cwd(), ts.sys.fileExists, ctx.config || 'tsconfig.json');
+          const configFile = ts.readConfigFile(configFileName, ts.sys.readFile);
+          const compilerOptions = ts.parseJsonConfigFileContent(configFile.config, ts.sys, path.dirname(configFileName));
+
+          tsConfig = compilerOptions.options;
+          tsConfig.inlineSourceMap = true;
+          if (!tsConfig.module) tsConfig.module = ts.ModuleKind.ESNext;
+        }
+
+        const { outputText } = ts.transpileModule(String(source), {
+          compilerOptions: tsConfig,
+          fileName: ctx.url,
+        });
+        return { source: outputText };
       }
     }
   ]
--- src/backend/controllers/genshin/app/TestRouter.ts
+++ src/backend/controllers/genshin/app/TestRouter.ts
@@ -1,10 +1,12 @@
 import { Request, Response, Router } from 'express';
 import { create } from '../../../routing/router.ts';
 import VuePageTest from '../../../components/VuePageTest.vue';
+
 export default async function(): Promise<Router> {
   const router: Router = create();
 
   router.get('/vue-test', async (req: Request, res: Response) => {
+    console.log('foobar');
     res.render(VuePageTest, {
       msg: 'hello world!',
       title: 'Vue Test'
