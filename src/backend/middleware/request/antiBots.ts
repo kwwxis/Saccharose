8a42373ecd17debc5ff80462816330b33d788bca
kwwxis <118708201+kwwxis@users.noreply.github.com>
2024-01-27 15:13:19 -0800

    Update

--- src/backend/middleware/request/antiBots.ts
+++ src/backend/middleware/request/antiBots.ts
@@ -2,10 +2,16 @@ import { NextFunction, Request, Response } from 'express';
 
 export const BAD_PATH_REGEX: RegExp = /\/wp-|\baws\b|\.env|cgi-|php|\.git|\.asp|\.cgi|\.axd|\.bak|\.html|geoserver|boaform|reportserver|(health|web)[_\-]?check|telescope|\bjira\b|microsoft/i;
 export const BAD_URI_REGEX: RegExp = /:80\/|:443\//g;
+export const VALID_HOSTS: Set<string> = new Set(['saccharose.wiki', 'www.saccharose.wiki', 'saccharose.localhost']);
 
 export default (req: Request, res: Response, next: NextFunction) => {
-  console.log('AntiBots URL: ', req.path, req.url, req.headers?.host);
-  if (BAD_PATH_REGEX.test(req.path) || BAD_URI_REGEX.test(req.url)) {
+  if (!req.headers?.host) {
+    res.status(400).send('Bad request');
+  } else if (!VALID_HOSTS.has(req.headers.host)) {
+    res.status(400).send('Bad request');
+  } else if (req.protocol !== 'https' && process.env.NODE_ENV === 'production') {
+    res.status(400).send('Bad request');
+  } else if (BAD_PATH_REGEX.test(req.path) || BAD_URI_REGEX.test(req.url)) {
     res.status(400).send('Bad request');
   } else {
     next();
