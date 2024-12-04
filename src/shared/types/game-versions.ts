2c78f5423716656c820651f2a3a756b66885f208
kwwxis <118708201+kwwxis@users.noreply.github.com>
2024-12-03 21:27:14 -0800

    uh

--- src/shared/types/game-versions.ts
+++ src/shared/types/game-versions.ts
@@ -62,6 +62,7 @@ export const StarRailVersions: GameVersion[] = [
   {number: '2.4', previous: '2.3'},
   {number: '2.5', previous: '2.4'},
   {number: '2.6', previous: '2.5'},
+  {number: '2.7', previous: '2.6'},
 ];
 
 // TODO: This needs to be updated with each new Zenless Zone Zero version!
@@ -92,7 +93,7 @@ export function isGameVersion(o: any): o is GameVersion {
 export const CurrentGenshinVersion: GameVersion = GenshinVersions.find(v => v.number === '5.2');
 
 // TODO: This needs to be updated with each new Honkai Star Rail version!
-export const CurrentStarRailVersion: GameVersion = StarRailVersions.find(v => v.number === '2.6');
+export const CurrentStarRailVersion: GameVersion = StarRailVersions.find(v => v.number === '2.7');
 
 // TODO: This needs to be updated with each new Zenless Zone Zero version!
 export const CurrentZenlessVersion: GameVersion = ZenlessVersions.find(v => v.number === '1.3');
