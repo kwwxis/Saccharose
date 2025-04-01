56028ef0e0616749df6183910e94285fba131b53
kwwxis <118708201+kwwxis@users.noreply.github.com>
2025-03-31 18:46:33 -0700

    update

--- src/backend/components/auth/UserLandingPage.vue
+++ src/backend/components/auth/UserLandingPage.vue
@@ -12,6 +12,20 @@
       </div>
     </div>
   </section>
+  <section class="card">
+    <h2 style="text-align: center">Rebranding Announcement: "nickjr.wiki"</h2>
+    <div class="content">
+      <p class="spacer15-bottom" style="font-size: 16px;line-height: 25px;max-width: 650px;margin: 0 auto;">After careful consideration and extensive research, I have determined that "saccharose.wiki" simply does not
+        capture the true essence of our wiki community. In recognition of the invaluable contributions, unwavering dedication,
+        and undeniable aura of sheer wiki mastery exhibited by one of the most esteemed editors, I am proud to announce the rebranding to <strong>nickjr.wiki</strong>.</p>
+
+      <p class="spacer15-bottom" style="font-size: 16px;line-height: 25px;max-width: 650px;margin: 0 auto;">This decision was made following a rigorous selection process in which various alternative names were proposed,
+        debated, and ultimately discarded in favor of the only logical choice. I believe this change better reflects
+        the core values of our wiki: knowledge, collaboration, and, most importantly, the overwhelming and inescapable presence of <strong>SolarMist/nickjr</strong>.</p>
+
+      <p style="font-size: 16px;line-height: 25px;max-width: 650px;margin: 0 auto;">I look forward to this new chapter in the tool's history and appreciate your support in embracing the nickjr.wiki era.</p>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
