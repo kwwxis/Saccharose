20dbaad2f5c30f62d02be6d6614edcbaa4a6accb
kwwxis <118708201+kwwxis@users.noreply.github.com>
2025-04-22 17:37:06 -0700

    zzz textmap overwrite files

--- src/shared/types/game-versions.ts
+++ src/shared/types/game-versions.ts
@@ -82,7 +82,7 @@ export const ZenlessVersions: GameVersion[] = [
   {number: '1.3', previous: '1.2'},
   {number: '1.4', previous: '1.3'},
   {number: '1.5', previous: '1.4'},
-  {number: '1.6', previous: '1.5'},g
+  {number: '1.6', previous: '1.5'},
   {number: '1.7', previous: '1.6'},
 ];
 
