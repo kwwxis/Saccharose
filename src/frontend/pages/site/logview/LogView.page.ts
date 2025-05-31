d3bf313a17ef584b3b8f2fa2864f7ee7a76375b7
kwwxis <118708201+kwwxis@users.noreply.github.com>
2025-05-31 16:54:19 -0700

    Update

--- src/frontend/pages/site/logview/LogView.page.ts
+++ src/frontend/pages/site/logview/LogView.page.ts
@@ -2,7 +2,7 @@ import { pageMatch } from '../../../core/pageMatch.ts';
 import { wsc } from '../../../websocket/wsclient.ts';
 import { LogViewLine, LogViewRequest } from '../../../../shared/types/wss-types.ts';
 import { LogViewEntity } from '../../../../shared/types/site/site-logview-types.ts';
-import { frag, frag1 } from '../../../util/domutil.ts';
+import { frag, frag1, getScrollbarWidth } from '../../../util/domutil.ts';
 import './logview.scss';
 import { escapeHtml } from '../../../../shared/util/stringUtil.ts';
 import { humanTiming, printHumanTiming } from '../../../../shared/util/genericUtil.ts';
@@ -14,15 +14,16 @@ pageMatch('vue/SiteLogViewPage', () => {
   const mainContainer: HTMLDivElement = document.querySelector('#site-logview');
   const myId: string = uuidv4();
 
+  const scrollbarWidth = getScrollbarWidth();
   mainContainer.append(frag(`
-    <div class="logview-row for-header">
+    <div class="logview-row for-header" style="margin-right:${scrollbarWidth}px">
       <div class="logview-cell for-timestamp">Timestamp</div>
       <div class="logview-cell for-wiki-user">Wiki User</div>
       <div class="logview-cell for-mode" title="(Input Lang):(Output Lang):(Search Mode)">User Modes</div>
       <div class="logview-cell for-http-status">HTTP Status</div>
       <div class="logview-cell for-http-method">HTTP Method</div>
       <div class="logview-cell for-http-uri">HTTP URI</div>
-      <div class="logview-cell for-http-runtime">HTTP Runtime</div>
+      <div class="logview-cell for-http-runtime" title="In milliseconds">HTTP Runtime</div>
     </div>
     <div id="site-logview-body"></div>
   `));
@@ -74,11 +75,14 @@ pageMatch('vue/SiteLogViewPage', () => {
         cssClasses.push(`for-wuwa`);
         cssClasses.push(`for-wuwa-${apiOrWeb}`);
       }
+      if (!fromRequest) {
+        cssClasses.push('flash');
+      }
 
       elements.push(frag1(`
         <div id="logview-${line.sha_hash}" class="logview-row ${cssClasses.join(' ')}" data-hash="${line.sha_hash}">
           <div class="logview-cell for-timestamp">${printHumanTiming(line.timestamp)}</div>
-          <div class="logview-cell for-wiki-user">${escapeHtml(line.wiki_user)}</div>
+          <div class="logview-cell for-wiki-user">${escapeHtml(line.wiki_user || '(Guest)')}</div>
           <div class="logview-cell for-mode">${escapeHtml(line.lang_in)}:${escapeHtml(line.lang_out)}:${escapeHtml(line.search_mode)}</div>
           <div class="logview-cell for-http-status">${escapeHtml(line.http_status)}</div>
           <div class="logview-cell for-http-method">${escapeHtml(line.http_method)}</div>
