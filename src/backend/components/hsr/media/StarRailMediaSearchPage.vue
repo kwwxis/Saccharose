d3b8048a00e30d727190749f4ce751f570c17e9e
kwwxis <118708201+kwwxis@users.noreply.github.com>
2025-08-30 23:55:39 -0700

    Update

--- src/backend/components/hsr/media/StarRailMediaSearchPage.vue
+++ src/backend/components/hsr/media/StarRailMediaSearchPage.vue
@@ -15,6 +15,14 @@
         </div>
         <SearchModeInput />
       </div>
+      <div class="valign">
+        <input id="firstVersionFilter" type="text" placeholder="Optional versions separated by comma or semicolon"
+               style="max-width: 490px;width: 100%;"/>
+        <span ui-tippy="{content: 'Only filters images to the version it was first added. Does not account for modifications.',delay:[200, 100]}"
+              class="valign spacer10-left" style="opacity: 0.5; width: 19px;">
+          <Icon name="info" />
+        </span>
+      </div>
       <div class="field valign spacer10-top">
         <button class="image-name-search-submit primary primary--2">Search</button>
         <div class="image-name-search-submit-pending hide loading small spacer5-left"></div>
