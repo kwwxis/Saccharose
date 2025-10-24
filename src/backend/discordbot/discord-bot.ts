09632ca4da5698bd711fc57f40acbe352083f4fb
kwwxis <118708201+kwwxis@users.noreply.github.com>
2025-10-24 00:15:32 -0700

    Update

--- src/backend/discordbot/discord-bot.ts
+++ src/backend/discordbot/discord-bot.ts
@@ -13,7 +13,7 @@ import { SiteUser } from '../../shared/types/site/site-user-types.ts';
 import {
   APIApplicationCommandOptionChoice
 } from 'discord-api-types/payloads/v10/_interactions/_applicationCommands/_chatInput/shared';
-import { LANG_CODES, LangCode } from '../../shared/types/lang-types.ts';
+import { DEFAULT_LANG, LANG_CODES, LangCode } from '../../shared/types/lang-types.ts';
 import { AvailableSiteModes, SiteMode } from '../../shared/types/site/site-mode-type.ts';
 import { OLResult } from '../../shared/types/ol-types.ts';
 import { ol_gen } from '../domain/abstract/basic/OLgen.ts';
@@ -23,6 +23,7 @@ import { getZenlessControl } from '../domain/zenless/zenlessControl.ts';
 import { getWuwaControl } from '../domain/wuwa/wuwaControl.ts';
 import { getStarRailControl } from '../domain/hsr/starRailControl.ts';
 import { SbOut } from '../../shared/util/stringUtil.ts';
+import { DEFAULT_SEARCH_MODE } from '../../shared/util/searchUtil.ts';
 
 const LANG_CHOICES: APIApplicationCommandOptionChoice<string>[] = LANG_CODES.filter(langCode => langCode !== 'CH')
   .map(langCode => ({
@@ -44,6 +45,7 @@ try {
       .setDescription('Change your Saccharose.wiki user options')
       .addSubcommand(command =>
         command.setName('game')
+          .setDescription('Set your game for commands')
           .addStringOption(opt => opt.setName('game').setChoices([
             { name: 'Genshin', value: 'genshin' },
             { name: 'HSR', value: 'hsr' },
@@ -53,11 +55,17 @@ try {
       )
       .addSubcommand(command =>
         command.setName('inlang')
+          .setDescription('Set your input language')
           .addStringOption(opt => opt.setName('lang').setChoices(LANG_CHOICES).setRequired(true)),
       )
       .addSubcommand(command =>
         command.setName('outlang')
+          .setDescription('Set your output language')
           .addStringOption(opt => opt.setName('lang').setChoices(LANG_CHOICES).setRequired(true)),
+      )
+      .addSubcommand(command =>
+        command.setName('info')
+          .setDescription('Show your current game setting, input language, and output language')
       )
       .toJSON(),
     new SlashCommandBuilder()
@@ -160,6 +168,29 @@ async function handleChatInputCommand(user: SiteUser, command: ChatInputCommandI
       await command.reply({
         content: `Your preferred game has been set to **${langCode}**.`,
         ephemeral: true,
+      });
+    }
+
+    if (subcommand === 'info') {
+      const embed = new EmbedBuilder()
+        .setTitle('Saccharose.wiki User Info')
+        .addFields({
+          name: 'Game',
+          value: user.prefs.dbotSiteMode || 'unset'
+        }, {
+          name: 'Input Language',
+          value: user.prefs.inputLangCode || DEFAULT_LANG
+        }, {
+          name: 'Output Language',
+          value: user.prefs.outputLangCode || DEFAULT_LANG
+        }, {
+          name: 'Search Mode',
+          value: user.prefs.searchMode || DEFAULT_SEARCH_MODE
+        });
+
+      await command.reply({
+        embeds: [embed],
+        ephemeral: true
       });
     }
   }
